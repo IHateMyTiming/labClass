56f32b85b5e3236c1406d8a2f063035045b06538
edgardpacheco007 <72029558+edgardpacheco007@users.noreply.github.com>
2020-11-16 16:12:48 +0000

    Update method.java

--- method.java
+++ method.java
@@ -1,9 +1,19 @@
 public static void main(String[] args) {
     
+
+
+
+
+static void EdgardMethod{    
 //EdgardMethod 
 
     int n1 = 10;
     int n2 = 5;
     s = n1+n2;
-    System.out.println(s);      
+ 
+    System.out.println("the some is:" + s);      
+}
+
+
+
 }
