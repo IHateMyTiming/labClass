a2287e4491a243808a8a5b0303e8874cb9ff8b2b
luizaalbuquerque <72030187+luizaalbuquerque@users.noreply.github.com>
2020-11-16 16:14:41 +0000

    Update method.java

--- method.java
+++ method.java
@@ -1,19 +1,28 @@
 public static void main(String[] args) {
     
-
-
-
-
-static void EdgardMethod{    
 //EdgardMethod 
 
     int n1 = 10;
     int n2 = 5;
     s = n1+n2;
- 
-    System.out.println("the some is:" + s);      
-}
-
-
-
+    System.out.println(s);      
+} 
+public static void main(String[] args) {
+    
+     // LuizaMethod
+    
+    int num1, num2, sum;
+    
+        Scanner sc = new Scanner(System.in);
+    
+        System.out.println("Please type a number: ");
+        num1 = sc.nextInt();
+        
+        System.out.println("Please type another number: ");
+        num2 = sc.nextInt();
+        
+        sc.close();
+        sum = num1 + num2;
+        System.out.println("Sum of these numbers: "+sum);
+    }
 }
